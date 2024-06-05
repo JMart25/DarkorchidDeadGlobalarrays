a1cd8cb0fa16e05418a1ca9fc31aaefe6b0036b6
JMart25 <149201983+JMart25@users.noreply.github.com>
2024-06-05 17:18:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Twitter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your message:");
+        string message = Console.ReadLine();
+
+        if (message.Length <= 140)
+        {
+            Console.WriteLine("Posted");
+        }
+        else
+        {
+            Console.WriteLine("Rejected");
+        }
+    }
+}
